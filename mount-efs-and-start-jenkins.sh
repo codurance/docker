52869267197ad3aa6ed2413d66beff0ff8b267c9
Cyryl Płotnicki <cyplo@cyplo.net>
2017-10-13 16:33:30 +0100

    New EFS server

--- mount-efs-and-start-jenkins.sh
+++ mount-efs-and-start-jenkins.sh
@@ -7,7 +7,7 @@ JENKINS_USER=$1
 echo "Current User is $CURRENT_USER"
 echo "Jenkins User is $JENKINS_USER"
 echo "Trying to mount..."
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-5240e29b.efs.eu-west-1.amazonaws.com:/ $JENKINS_HOME
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-9303a15a.efs.eu-west-1.amazonaws.com:/ $JENKINS_HOME
 chown -R $JENKINS_USER "$JENKINS_HOME" /usr/share/jenkins/ref
 echo "Mounted."
 echo "Running jenkins..."
